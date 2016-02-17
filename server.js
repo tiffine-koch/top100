c5edcb9945318e74b5959fc427faeed756ebdf4e
Apple <tiffine.koch@gmail.com>
2016-02-17 15:03:43 -0800

    added stormpath

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var express = require('express');
+var stormpath = require('express-stormpath');
 // var topMovies = require('./routes/api/top-movies');
 var api = require('./routes/api');
 
 var app = express();
 
+app.use(stormpath.init());
 app.use('/api', api);
 
 app.listen(process.env.PORT || 3000);
