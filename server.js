0c0f1821448797af12a2eaea0522c9ab94beba30
Apple <tiffine.koch@gmail.com>
2016-02-17 16:20:15 -0800

    added stormpath

--- server.js
+++ server.js
@@ -7,10 +7,37 @@ var api = require('./routes/api');
 
 var app = express();
 
-app.use(stormpath.init());
-app.use('/api', api);
+app.use(stormpath.init(app, {
+  website: true,
+  expand: {
+    customData: true
+    }
+}));
 
-app.listen(process.env.PORT || 3000);
+app.use('/api', stormpath.apiAuthenticationRequired, api);
+
+app.get('createapikey', stormpath.loginRequired, (req, res, next) => {
+  req.user.createApiKey((err, key) => {
+    res.json({ id: keyid, secret: key.secret});
+  });
+})
+// app.get('/', (req, res, next) => {
+//   res.user.href;
+//   {
+//     "product": "shazaam towel",
+//     "user": req.user.href
+//   }
+// });
+// app.get('/', stormpath.loginRequired, (req, res, next) => {
+//   req.user.getCustomData((err, data) => {
+//     console.log(data);
+//     res.send("Hi" + req.user.fullName);
+// });
+
+app.on('stormpath.ready', () => {
+  app.listen(process.env.PORT || 3000);
+  console.log('server is ready');
+});
 
 /*
 *API SPEC DISCUSSION
@@ -18,3 +45,23 @@ app.listen(process.env.PORT || 3000);
 * GET mywebsite.com/api/top-movies //get a list of top movies
 * POST mywebsite.com/api/top-movies //get a list of top movies
 */
+
+// app.get('/', (req, res, next) => {
+//   if(req.user) {
+//     req.user.getCustomData((err, data) => {
+//       if(err) {
+//         return next(err);
+//       }
+//       data.favoriteColor = 'black';
+//       data.address = {
+//         city: 'Fremont',
+//         school: 'Coding House'
+//         };
+//       data.save();
+//     });
+//     console.log(req.user);
+//     res.send("Hi: " + req.user.givenName + '. You are now logged in!');
+//   } else {
+//     res.send("HEY! LOG IN PLZ");
+//   }
+// });
